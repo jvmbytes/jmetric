8580258d461354e40c746cbe90033319e7ee1fc0
wangoo <wongoo@apache.org>
2021-01-11 13:47:07 +0800

    support 8u73

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM openjdk:<VERSION>
+FROM <BASE_IMAGE>
 COPY target/jmetric-1.0.0-SNAPSHOT.jar jmetric-1.0.0.jar
 CMD ["java", "-jar", "jmetric-1.0.0.jar"]
--- build-docker-image.sh
+++ build-docker-image.sh
@@ -6,13 +6,14 @@ mvn clean package
 
 function build_docker_image() {
     ver=$1
+    baseImage=$2
 
     cp Dockerfile target/Dockerfile
 
     if [ "$(uname)" == "Darwin" ]; then
-      sed -i "" "s/<VERSION>/$ver/g" target/Dockerfile
+      sed -i "" "s#<BASE_IMAGE>#$baseImage#g" target/Dockerfile
     else
-      sed -i"" "s/<VERSION>/$ver/g" target/Dockerfile
+      sed -i"" "s#<BASE_IMAGE>#$baseImage#g" target/Dockerfile
     fi
 
     docker build -t jmetric:$ver -f target/Dockerfile .
@@ -21,8 +22,9 @@ function build_docker_image() {
     if [ $withPush == true ]; then docker push jvmbytes/jmetric:$ver; fi
 }
 
-build_docker_image 8-jre
-build_docker_image 8u275
-build_docker_image 11-jre
+build_docker_image 8u73 lwieske/java-8:jdk-8u73
+build_docker_image 8-jre openjdk:8-jre
+build_docker_image 8u275 openjdk:8u275
+build_docker_image 11-jre openjdk:11-jre
 
 
